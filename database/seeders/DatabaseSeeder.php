0f691a77d6f96a164cb95c09114f10ce7fdc6fc1
Prakash Pandey <prakashkpandey2059@gmail.com>
2026-04-17 11:40:51 +0545

    add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Prakash Pandey',
-            'email' => 'prakashpandey@gmail.com',
-            'password' => bcrypt('prakashpandey'),
+            'name' => 'Bihani Tech',
+            'email' => 'info@bihanitech.com',
+            'password' => bcrypt('Bihanitech1234'),
         ]);
     }
 }
