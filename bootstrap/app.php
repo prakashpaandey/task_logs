b7d7d8258e07c8cbfacd22719d79e6ecc11be945
Prakash Pandey <prakashkpandey2059@gmail.com>
2026-05-06 15:17:40 +0545

    remove ngrok setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');       
         $middleware->web(append: [
             \App\Http\Middleware\EnsureUserIsActive::class,
         ]);
